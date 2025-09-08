a30028e95a7ab80f8769970acea750bf93d365f4
JESLIN <jj20092005@gmail.com>
2025-09-08 18:17:40 +0530

    poseidon and end congratulations page

--- frontend/src/pages/Poseidon.jsx
+++ frontend/src/pages/Poseidon.jsx
@@ -1,5 +1,21 @@
-import ChallengePage from "../components/ChallengePage";
-const NAME="poseidon", NUMBER=5, TITLE="Poseidon";
-const HASH="1e2c75f343da0f688fc45b6e5e11d8992406811172f9bc4af36f07bacbaccf56";
-const CODE="Z";
-export default function Poseidon(){return <ChallengePage title={TITLE} name={NAME} number={NUMBER} hash={HASH} code={CODE} nextRoute="/challenge/Zeus" />;}
+import ChallengePageHiddenError from "../components/PChallenge"; // Adjust path as needed
+
+const NAME = "poseidon",
+  NUMBER = 5,
+  TITLE = "Poseidon";
+
+const HASH = "1e2c75f343da0f688fc45b6e5e11d8992406811172f9bc4af36f07bacbaccf56";
+const CODE = "Z";
+
+export default function Poseidon() {
+  return (
+    <ChallengePageHiddenError
+      title={TITLE}
+      name={NAME}
+      number={NUMBER}
+      hash={HASH}
+      code={CODE}
+      nextRoute="/challenge/Zeus"
+    />
+  );
+}
